b57aa74b2732a649e7dd35e55e830b610c37c192
amanverasia <verasiaaman3@gmail.com>
2024-12-02 21:24:52 +0530

    sync some more

--- Day 1/1.py
+++
@@ -1,8 +0,0 @@
-data = []
-with open ("Day 1/number.txt", "r") as f:
-    lines = f.readlines()
-    for line in lines:
-        data.append(line.split('\n')[0])
-
-for line in data:
-    print(line)
